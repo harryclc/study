cff7ce34825e30ed5f681b276128c706331fa03e
Harry <harryclc@users.noreply.github.com>
2016-06-02 14:36:55 +0800

    Update ZijingThank.user.js

--- javascript/ZijingThank.user.js
+++ javascript/ZijingThank.user.js
@@ -7,16 +7,16 @@
 // @require     http://code.jquery.com/jquery-2.1.3.min.js
 // @grant       none
 // @run-at      document-end
-// @version     0.3
+// @version     0.4
 // ==/UserScript==
 
 $("span.top_state").append(' 待感谢: <span class="green" id="thank_count">'+ $("a.index_thanks").length + '</span>');
 var int = setInterval(function c(){
-    if( $("a.index_thanks").length == 0 ){
+    if( $("a.index_thanks").length === 0 ){
         clearInterval(int);
-		$("#thank_count").html( $("a.index_thanks").length + "/" + $("#thank_count").html() );
+	$("#thank_count").html( $("a.index_thanks").length + "/" + $("#thank_count").html() );
     }
     else{
         $("a.index_thanks").get(0).click();
     }
-},100)
+},100);
